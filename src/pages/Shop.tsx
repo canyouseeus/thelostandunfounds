b6928cbd22e8d74e35fd2b0af61edb8065a47cd3
canyouseeus <canyouseeus@users.noreply.github.com>
2025-11-16 15:43:41 -0600

    Update product grid to 3 columns max

--- src/pages/Shop.tsx
+++ src/pages/Shop.tsx
@@ -228,7 +228,7 @@ export default function Shop() {
       )}
 
       {products.length > 0 && (
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
           {products.map((product) => (
             <a
               key={product.id}
